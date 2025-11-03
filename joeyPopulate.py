3903e435d5b97228e7c36166bb6ffc0ca7d50f19
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2025-11-03 15:00:25 -0600

    Added German Populate
    Itialian populate will basically be the same

--- joeyPopulate.py
+++ joeyPopulate.py
@@ -36,12 +36,53 @@ for counter in range(1, 301):
         swId = counter - 200
     
     smt = f"INSERT INTO WordID VALUES ({swId}, {counter}, '{language}')"
-    print(smt)
+    
+    #print(smt)
     cursor.execute(smt)
 
 connection.commit()
 
 
+# populate English
+
+english_file = open("EnglishSwadeshList", "r")
+english_words = [line.rstrip() for line in english_file]
+english_file.close()
+
+for wordId in range(1, 101):
+    smt = f"INSERT INTO English VALUES ({wordId}, '{english_words[wordId - 1]}')"
+    
+    #print(smt)
+    cursor.execute(smt)
+
+connection.commit()
+
+# set up German and Itialian
+
+deu_and_ita_file = open("GermanAndItalianWithGerndersIncluded")
+deu_and_ita = [line.rstrip() for line in deu_and_ita_file]
+deu_and_ita_file.close()
+
+deu_words = [deu_and_ita[index] for index in range(100)]
+ita_words = [deu_and_ita[index] for index in range(100, 200)]
+
+# populare German
+
+for wordId in range(100, 200):
+    current_word = deu_words[wordId - 100]
+    smt = ''
+
+    if ' ' in current_word:
+        word_array = current_word.split(" ")
+        smt = f"INSERT INTO German VALUES ({wordId}, '{word_array[0]}', '{word_array[1]}')"
+    else:
+        smt = f"INSERT INTO German (WordID_wordID, translation) VALUES ({wordId}, '{current_word}')"
+
+    print(smt)
+    cursor.execute(smt)
+
+
+connection.commit()
 
 cursor.close()
 connection.close()
