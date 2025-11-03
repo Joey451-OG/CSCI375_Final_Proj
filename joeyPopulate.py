9a23fb9adbc961b031d9acf82e257fb18d7f5dd7
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2025-11-03 10:35:45 -0600

    Swadesh and WordID generated
    Need the next 4 tables (all langs, and POS)

--- joeyPopulate.py
+++ joeyPopulate.py
@@ -19,8 +19,29 @@ cursor = connection.cursor()
 for _ in range(100):
     cursor.execute("INSERT INTO Swadesh VALUES ()")
 
+connection.commit()
+
+# populate WordID
+for counter in range(1, 301):
+    language = ''
+    swId = counter
+
+    if counter <= 100 and not counter > 100:
+        language = 'eng'
+    if counter > 100 and not counter > 200:
+        language = 'deu'
+        swId = counter - 100
+    if counter > 200:
+        language = 'ita'
+        swId = counter - 200
+    
+    smt = f"INSERT INTO WordID VALUES ({swId}, {counter}, '{language}')"
+    print(smt)
+    cursor.execute(smt)
 
 connection.commit()
+
+
 
 cursor.close()
 connection.close()
